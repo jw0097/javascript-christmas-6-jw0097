3e2bc05b0f6160ae4527c8d4bc0721d2773a9ab5
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-12 16:22:13 +0900

    refactor: 요일 계산, 음식 카운트 계산을 상속받을 수 있도록 코드 작성

--- src/Model/Event/RestaurantEvent.js
+++ src/Model/Event/RestaurantEvent.js
@@ -4,8 +4,19 @@ class RestaurantEvent {
       throw new Error("RestaurantEvent 인스턴스를 생성할 수 없습니다.");
     }
   }
+
   implement() {
     throw new Error("implement를 오버라이딩하고 사욯해야 합니다.");
+  }
+
+  _calculateDayFromDate(date) {
+    return new Date(2023, 11, date).getDay();
+  }
+
+  _calculateDishCount(menuList, dish) {
+    return Object.entries(menuList).reduce((dishCount, [menu, count]) => {
+      return Object.keys(dish).includes(menu) ? dishCount + count : dishCount;
+    }, 0);
   }
 }
 
