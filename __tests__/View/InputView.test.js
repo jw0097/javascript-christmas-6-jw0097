f129967ea61fcacf45bdccb25f004800469fc800
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-12 18:01:07 +0900

    test: 사용자에게 메뉴를 입력받는 기능에 대한 테스트 코드 작성

--- __tests__/View/InputView.test.js
+++ __tests__/View/InputView.test.js
@@ -25,4 +25,30 @@ describe("InputView 객체 테스트", () => {
       expect(date).toBe(numberedDate);
     });
   });
+
+  describe("readMenuList 메서드 테스트", () => {
+    test("readMenuList 메서드가 존재해야 한다.", () => {
+      expect(typeof InputView.readMenuList).toBe("function");
+    });
+
+    test("readMenuList 메서드가 호출되면 Console.readLineAsync가 호출되야 한다.", () => {
+      InputView.readMenuList();
+      expect(Console.readLineAsync).toBeCalledWith(INPUT_MESSAGE.menuList);
+    });
+
+    test("readMenuList 메서드가 promise를 반환하고 그 resolve 값이 입력값이어야 한다.", () => {
+      const menuListInput = "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1";
+      const expectedMenuList = {
+        티본스테이크: 1,
+        바비큐립: 1,
+        초코케이크: 2,
+        제로콜라: 1,
+      };
+      const resolvedPromise = Promise.resolve(menuListInput);
+      Console.readLineAsync.mockReturnValue(resolvedPromise);
+
+      const menuList = InputView.readMenuList();
+      expect(menuList).toEqual(expectedMenuList);
+    });
+  });
 });
