4e377c751c3f4f0f1cd1d003bc298a0c1831f0b8
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-12 17:20:30 +0900

    feat: 증정 이벤트 기능 구현

--- src/Model/Event/Giveaway.js
+++ src/Model/Event/Giveaway.js
@@ -1,3 +1,10 @@
-class Giveaway {}
+import RestaurantEvent from "./RestaurantEvent";
+
+class Giveaway extends RestaurantEvent {
+  implement({ totalAmount }) {
+    if (totalAmount < 120000) return;
+    return { giveaway: "샴페인" };
+  }
+}
 
 export default Giveaway;
