bbd24dd2436005bd8dfbbffa4bd51b38d5563eec
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-13 16:27:15 +0900

    test: 이벤트 적용 기능에 대한 테스트 코드 작성

--- __tests__/Model/Restaurant.test.js
+++ __tests__/Model/Restaurant.test.js
@@ -13,13 +13,7 @@ let restaurant;
 describe("Restaurant 클래스 테스트", () => {
   beforeEach(() => {
     restaurant = new Restaurant({
-      events: [
-        new XmasDdayDiscount(),
-        new WeekdayDiscount(),
-        new WeekendDiscount(),
-        new SpecialDiscount(),
-        new Giveaway(),
-      ],
+      events: [new XmasDdayDiscount(), new WeekdayDiscount(), new WeekendDiscount(), new SpecialDiscount(), new Giveaway()],
       badgeAward: new BadgeAward(),
     });
   });
@@ -40,6 +34,30 @@ describe("Restaurant 클래스 테스트", () => {
       restaurant.saveCustomerInfo({ date, menuList });
 
       expect(Customer).toBeCalledWith({ date, menuList });
+    });
+  });
+
+  describe("implementEvent 메서드 테스트", () => {
+    test("implementEvent 메서드가 존재해야 한다.", () => {
+      expect(typeof restaurant.implementEvent).toBe("function");
+    });
+
+    test("implementEvet 메서드를 호출하면 이벤트 적용 정보를 반환해야 한다.", () => {
+      const orderInfo = {
+        date: 3,
+        menuList: { 티본스테이크: 1, 바비큐립: 1, 초코케이크: 2, 제로콜라: 1 },
+      };
+      const eventInfo = {
+        totalAmount: 142000,
+        giveaway: "샴페인",
+        xmasDdayDiscount: 1200,
+        weekdayDiscount: 4046,
+        specialDiscount: 1000,
+        eventImplementAmount: 31246,
+        badgeAward: "산타",
+      };
+      const eventImplementInfo = restaurant.implementEvent(orderInfo);
+      expect(eventImplementInfo).toEqual(eventInfo);
     });
   });
 });
