8576d8be387f90b2745b04f1f78c9a711464c0df
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-13 16:35:30 +0900

    test: 배지 정보를 고객 정보에 저장 하는 기능에 대한 테스트 코드 작성

--- __tests__/Model/Customer.test.js
+++ __tests__/Model/Customer.test.js
@@ -1,3 +1,17 @@
 import Customer from "../../src/Model/Customer";
 
-describe("Customer 클래스 테스트", () => {});
+let customer;
+describe("Customer 클래스 테스트", () => {
+  beforeEach(() => {
+    const orderInfo = {
+      date: 3,
+      menuList: { 티본스테이크: 1, 바비큐립: 1, 초코케이크: 2, 제로콜라: 1 },
+    };
+    customer = new Customer(orderInfo);
+  });
+  describe("setBadge 메서드 테스트", () => {
+    test("Customer 클래스에 setBadge 메서드가 존재해야 한다.", () => {
+      expect(typeof customer.setBadge).toBe("function");
+    });
+  });
+});
