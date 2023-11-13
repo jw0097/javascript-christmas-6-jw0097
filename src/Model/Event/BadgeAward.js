64771b249fcaf8f5253fa78472c2c7b8164eb1b1
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-13 16:05:38 +0900

    feat: 배지 부여 기능 구현

--- src/Model/Event/BadgeAward.js
+++ src/Model/Event/BadgeAward.js
@@ -1,3 +1,18 @@
-class BadgeAward {}
+import { BADGE_AWARD_CONDITION } from "../../constant/restaurant";
+
+class BadgeAward {
+  implement(eventImplementAmount) {
+    const badgeAward = Object.entries(BADGE_AWARD_CONDITION).reduce(
+      (acc, [badge, [min, max]]) => {
+        if (eventImplementAmount >= min && eventImplementAmount < max)
+          acc = badge;
+        return acc;
+      },
+      undefined
+    );
+
+    return badgeAward && { badgeAward };
+  }
+}
 
 export default BadgeAward;
