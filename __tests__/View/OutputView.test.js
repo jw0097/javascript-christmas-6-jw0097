2ae00da7c0d6f63ab13ed0537f6a5d752736be6a
jw0097 <76507017+jw0097@users.noreply.github.com>
2023-11-14 13:23:56 +0900

    test: 이벤트 적용 결과 출력 기능에 대한 테스트 코드 작성

--- __tests__/View/OutputView.test.js
+++ __tests__/View/OutputView.test.js
@@ -4,6 +4,9 @@ import OutputView from "../../src/View/OutputView";
 Console.print = jest.fn();
 
 describe("OutputView 객체 테스트", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
   describe("printMessage 메서드 테스트", () => {
     test("printMessage 메서드가 존재해야 한다.", () => {
       expect(typeof OutputView.printMessage).toBe("function");
@@ -14,6 +17,58 @@ describe("OutputView 객체 테스트", () => {
       OutputView.printMessage(message);
 
       expect(Console.print).toBeCalledWith(message);
+    });
+  });
+
+  describe("printEventImplementInfo 메서드 테스트", () => {
+    test("printEventImplementInfo 메서드가 존재해야 한다.", () => {
+      expect(typeof OutputView.printEventImplementInfo).toBe("function");
+    });
+
+    test("printEventImplementInfo 메서드가 호출되면 Console.print 메서드가 이벤트 결과와 호출되야 한다.", () => {
+      const menuList = { 티본스테이크: 1, 바비큐립: 1, 초코케이크: 2, 제로콜라: 1 };
+      const eventImplementInfo = {
+        totalAmount: 142000,
+        xmasDdayDiscount: 1200,
+        weekdayDiscount: 4046,
+        specialDiscount: 1000,
+        giveaway: "샴페인",
+        eventImplementAmount: 31246,
+        badgeAward: "산타",
+      };
+      const eventImplementInfoMessage = [
+        "",
+        "<주문 메뉴>",
+        "티본스테이크 1개",
+        "바비큐립 1개",
+        "초코케이크 2개",
+        "제로콜라 1개",
+        "",
+        "<할인 전 총주문 금액>",
+        "142,000원",
+        "",
+        "<증정 메뉴>",
+        "샴페인 1개",
+        "",
+        "<혜택 내역>",
+        "크리스마스 디데이 할인: -1,200원",
+        "평일 할인: -4,046원",
+        "특별 할인: -1,000원",
+        "증정 이벤트: -25,000원",
+        "",
+        "<총혜택 금액>",
+        "-31,246원",
+        "",
+        "<할인 후 예상 결제 금액>",
+        "135,754원",
+        "",
+        "<12월 이벤트 배지>",
+        "산타",
+      ].join("\n");
+
+      OutputView.printEventImplementInfo({ menuList, eventImplementInfo });
+
+      expect(Console.print).toBeCalledWith(eventImplementInfoMessage);
     });
   });
 });
